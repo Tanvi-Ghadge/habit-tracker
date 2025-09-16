312ae65bbeac9a47996f06b3635359d435f742db
tanvi ghadge <tanvighadge27@gmail.com>
2025-09-16 20:58:19 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -7,35 +7,45 @@ import authrouter from "./routes/auth.route.js";
 import habitrouter from "./routes/habit.route.js";
 import socialrouter from "./routes/social.route.js";
 import leaderboardRouter from "./routes/leaderboard.route.js";
+
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
+
 const allowedOrigins = [
-  "https://habit-tracker-ten-umber.vercel.app",
+  "https://habit-tracker-cy57.vercel.app",
   "http://localhost:5173",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps, Postman)
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("CORS not allowed for origin: " + origin));
       }
     },
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allow all methods
+    credentials: true, // allow cookies
   })
 );
 
+// Routes
 app.use("/api/auth", authrouter);
 app.use("/api/habit", habitrouter);
 app.use("/api/social", socialrouter);
 app.use("/api/game", leaderboardRouter);
-const PORT = process.env.PORT;
+
+
+app.options("*", cors());
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("Server is running on PORT: " + PORT);
   connectdb();
 });
